b1e9542934d1de30bf1eb394e956b1f83db4447f
valerrrich <161199056+valerrrich@users.noreply.github.com>
2024-05-02 14:23:56 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,33 @@
+    const data = generateRandomData();
+
+}
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+}
+    data.forEach(item => {
+function main() {
+
+main();
+    });
+    }
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    return data;        console.log("Random Number: " + item);
+}
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+
+function main() {
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+
+main();
+    data.forEach(item => {
+    return data;
+function generateRandomData() {
+    const data = generateRandomData();
+}
+    }
